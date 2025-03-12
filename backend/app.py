485b3c64118d10ba4b6afb5b709e64c6998c8365
HASSANE SKIKRI <hassaneskikri@gmail.com>
2025-03-12 17:12:51 +0000

    Add backend structure with Docker support and initial Flask API

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Welcome to Healthcare AI Diagnostic System API'})
+
+@app.route('/api/health', methods=['GET'])
+def health_check():
+    return jsonify(
+        {
+            'status': 'healthy',
+            'message': 'The Healthcare AI Diagnostic System API is up and running!'
+        }
+    )
+    
+if __name__ == '__main__':
+    app.run(debug=True,port=8000)
---
+++ backend/config.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY','my_production_secret_key')
+    DEBUG = True
+    
+    # Database
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(
+        os.environ.get("DB_USERNAME", 'postgres'),
+        os.environ.get("DB_PASSWORD", 'postgres'),
+        os.environ.get("DB_HOST", 'db'),
+        os.environ.get("DB_PORT", '5432'),
+        os.environ.get("DB_NAME", 'healthcare_ai')
+    )
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off update messages from sqlalchemy
+
+    
